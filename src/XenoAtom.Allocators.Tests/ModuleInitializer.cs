cfff8ba9779112388ca7d150dab0ce8295d4af79
Simon Cropp <simon.cropp@gmail.com>
2024-07-19 19:42:24 +1000

    update verify stuff

---
+++ src/XenoAtom.Allocators.Tests/ModuleInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+
+static class ModuleInit
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+        UseProjectRelativeDirectory("Verified");
+        DiffEngine.DiffRunner.Disabled = true;
+    }
+}
