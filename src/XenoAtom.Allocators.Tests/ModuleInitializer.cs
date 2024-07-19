464dafcf72a458a891d43ca991c608c3a65dce2c
Simon Cropp <simon.cropp@gmail.com>
2024-07-19 19:46:29 +1000

    DiffPlex

--- src/XenoAtom.Allocators.Tests/ModuleInitializer.cs
+++ src/XenoAtom.Allocators.Tests/ModuleInitializer.cs
@@ -1,11 +1,13 @@
 ﻿using System.Globalization;
 using System.Runtime.CompilerServices;
+using VerifyTests.DiffPlex;
 
 static class ModuleInit
 {
     [ModuleInitializer]
     public static void Initialize()
     {
+        VerifyDiffPlex.Initialize(OutputType.Compact);
         CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
         UseProjectRelativeDirectory("Verified");
         DiffEngine.DiffRunner.Disabled = true;
